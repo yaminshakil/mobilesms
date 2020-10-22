7e231e86c173a3e4dffe0bd3d260237ee08ca586
yaminshakil <42623165+yaminshakil@users.noreply.github.com>
2020-10-22 23:37:06 +0600

    Added phonesms

---
+++ app/SendCode.php
@@ -0,0 +1,16 @@
+<?php
+namespace App;
+class SendCode
+{
+    public static function sendCode($phone){
+        $code=rand(1111,9999);
+        $nexmo=app('Nexmo\Client');
+        $nexmo->message()->send([
+            'to'=>'+880'.(int) $phone,
+            'from'=> 'Yamin',
+            'text'=>'Verify code: '.$code,
+        ]);
+        return $code;
+    }
+
+}
